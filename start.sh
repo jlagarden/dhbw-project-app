ec93a5c2d2fbd21d00250e44436e99bb70f44b71
Helge Dickel <helge@dickel-online.de>
2016-10-20 16:10:27 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -jar TaktstrasseOpcServer-0.0.1-SNAPSHOT.jar  -o /Users/helgedickel/gitlab/dhbw-projekt-app/project-app/tmp -d 1000 -amqp tcp://127.0.0.1:61616 -kafka localhost:9092 -topic prod
